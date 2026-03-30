18fed44279402d230ff254ebdae6e660005734d4
Karim <karimkomi536@gmail.comm>
2026-03-30 19:11:14 +0100

    fix: switch import_2425 to api-sports.io host

--- scripts/python/import_2425.py
+++ scripts/python/import_2425.py
@@ -79,10 +79,9 @@ LEAGUES = {
     135: {"name": "Serie A",        "country": "Italy"},
 }
 
-API_BASE = "https://api-football-v1.p.rapidapi.com/v3"
+API_BASE = "https://v3.football.api-sports.io"
 HEADERS  = {
-    "x-rapidapi-key":  API_KEY,
-    "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+    "x-apisports-key": API_KEY,
 }
 
 _POS_MAP = {
